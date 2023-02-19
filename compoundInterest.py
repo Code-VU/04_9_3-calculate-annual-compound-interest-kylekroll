21bb3cd4411ebb78f28fe4e32805a08585b71d03
Kyle Kroll <Kyle.Kroll@vu.com>
2023-02-19 17:43:59 -0600

    Completed program to calculate compound interest

--- compoundInterest.py
+++ compoundInterest.py
@@ -1,11 +1,31 @@
+def interest_formula(p, r, t):
+    a = p * (1 + r / 100) ** t
+    print("Compound Interest: " + str(round(a - p, 2)))
+
+def collect_input():
+    client_principal = float(input("Principle (amount): "))
+    client_time = float(input("Time: "))
+    client_rate = float(input("Rate: "))
+    return client_principal, client_time, client_rate
+
 def calculateCompoundInterest():
+    line_break = "---"
+
+    client_principal_1, client_time_1, client_rate_1 = collect_input()
+    interest_formula(client_principal_1, client_rate_1, client_time_1)
+    print(line_break)
+
+    client_principal_2, client_time_2, client_rate_2 = collect_input()
+    interest_formula(client_principal_2, client_rate_2, client_time_2)
+    print(line_break)
+
+    client_principal_3, client_time_3, client_rate_3 = collect_input()
+    interest_formula(client_principal_3, client_rate_3, client_time_3)
+    
     
 # This first 3 lines are provided for yougetACompoundInterest()
 # This first 3 lines are provided for you
- client_one_principal = float(input("Principle (amount): "))
- client_one_time =      float(input("Time:               "))
- client_one_rate =      float(input("Rate:               "))
- #print("Compound Interest: "+str(intrest))
+
 
     # end assignment
 
